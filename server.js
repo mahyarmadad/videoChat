a0fd11977b7c0c7b34ff0676eb5c619cf884276d
mahyarmadad <erfansam8891@yahoo.com>
2021-07-22 10:56:50 +0430

    creat server

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const http = require("http");
 const port = process.env.PORT || 5000;
+const server = http.createServer(app);
 
 app.get("*", (req, res) => {
   res.send("hello from simple server :)");
 });
 
-app.listen(port, () =>
+server.listen(port, () =>
   console.log("> Server is up and running on port : " + port)
 );
